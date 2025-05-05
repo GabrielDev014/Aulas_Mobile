8fd318512991d22a7df37b6ec432d4b004bbe334
Gabriel <gabriellimaalves.si25@gmail.com>
2025-05-05 20:37:11 -0300

    aula carregar imagens

--- MyApplication/app/src/main/java/com/example/myapplication/Carregamento.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Carregamento.kt
@@ -1,15 +1,24 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 
 class Carregamento : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_carregamento)
+    }
+    fun CarregarImagem(view: View)
+    {
+        var url = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Emirates_logo.svg/1934px-Emirates_logo.svg.png"
+        var emirates = findViewById<ImageView>(R.id.emirates)
+        Glide.with(this).load(url).into(emirates)
     }
 }
